2156efd37d84753443f3f8a3f27eb9763e3055f4
mertovich <88425310+mertovich@users.noreply.github.com>
2022-05-23 19:17:34 +0300

    add(write): all_message.csv

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import pandas as pd
 import json
 from pandas import json_normalize
 
-
 data = json.loads(open('user.json').read())
 df = json_normalize(data) 
 df = pd.DataFrame(df, columns= ['message'])
 all_message = df.groupby(df.columns.tolist(),as_index=False).size()
-print(all_message.sort_values(by=['size'], ascending=False))
+all_message = all_message.sort_values(by=['size'], ascending=False)
+all_message.to_csv('all_message.csv')
