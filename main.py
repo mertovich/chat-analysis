157e22aa6c675c311713e59ab4f44c196882cc2a
mertovich <88425310+mertovich@users.noreply.github.com>
2022-05-25 15:54:36 +0300

    fix: column error

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from email import message
 import pandas as pd
 import json
 from pandas import json_normalize
@@ -7,5 +6,6 @@ data = json.loads(open('user.json').read())
 df = json_normalize(data) 
 df = pd.DataFrame(df, columns= ['message'])
 all_message = df.groupby(df.columns.tolist(),as_index=False).size()
-all_message = all_message.sort_values(by=['size'], ascending=False)
-all_message.to_csv('all_message.csv')
+all_message = all_message.sort_values(by=['size'],ascending=False)
+all_message.to_csv('all_message.csv', index=False, sep=';')
+print(all_message)
